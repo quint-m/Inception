8667003bce39582bdace03c8b9fd3d50f22dff36
Quinten <quinten@bumbal.eu>
2026-02-08 16:20:54 +0100

    remove: delete init.sql script for database and user creation

--- srcs/requirements/mariadb/tools/init.sql
+++
@@ -1,9 +0,0 @@
--- -- Create database if it doesn't exist
--- CREATE DATABASE IF NOT EXISTS wordpress;
-
--- -- Create user if it doesn't exist (MariaDB 10.1+)
--- CREATE USER IF NOT EXISTS 'wpuser'@'%' IDENTIFIED BY 'password';
-
--- -- Grant privileges
--- GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'%';
--- FLUSH PRIVILEGES;
