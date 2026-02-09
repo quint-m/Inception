8b3a45adc45b1a0fa3446b74085ea867146701e6
Quinten <quinten@bumbal.eu>
2026-02-09 20:48:31 +0100

    fix: update admin email variable in entrypoint script for consistency

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -12,7 +12,7 @@ if ! wp core is-installed 2>/dev/null; then
     wp core download --allow-root --path="/var/www/html"
     wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=$(cat /run/secrets/db_password) --dbhost=mariadb --allow-root
     wp db create --allow-root 2>/dev/null || echo "Database already exists"
-    wp core install --url=localhost --title="Inception" --admin_user=${WP_ADMIN} --admin_password=$(cat /run/secrets/wp_admin_password) --admin_email=qmennen@student.codam.nl --skip-email --allow-root
+    wp core install --url=localhost --title="Inception" --admin_user=${WP_ADMIN} --admin_password=$(cat /run/secrets/wp_admin_password) --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
     wp user create --allow-root $WP_AUTHOR $WP_AUTHOR_EMAIL --role=author --user_pass=$(cat /run/secrets/wp_password) --path=/var/www/html
 fi
 
